612053265b78465ad96d6ab892404ef39fe6aa8d
Nasrullah <naz.cules@gmail.com>
2020-07-27 09:12:22 +0800

    Utils: create utility files

---
+++ utils/index.js
@@ -0,0 +1,4 @@
+export { default as flattenDeep } from './flatten-deep'
+export { default as isDev } from './is-dev'
+export { default as ampify } from './ampify'
+export { default as isExists } from './is-exists'
