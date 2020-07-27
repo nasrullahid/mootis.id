05e34b7fd23900e99aee1915bb71cc14101a02e9
Nasrullah <naz.cules@gmail.com>
2020-07-27 14:07:23 +0800

    layout: implement component to  default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,88 @@
 <template>
-  <div>
+  <div id="top" class="mx-auto w-full">
+    <AppTopNav :office-time="officeTime" :social-network="socialNetwork" />
+    <AppHeader :logo="logo" :menu="menu" />
+    <AppCarousel />
     <Nuxt />
+    <AppFooter :contact="contact" :social-network="socialNetwork" />
+    <AppToTop />
   </div>
 </template>
 
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+<script>
+import AppToTop from '@/components/AppToTop'
+import AppTopNav from '@/components/Layout/AppTopNav'
+import AppHeader from '@/components/Layout/AppHeader'
+import AppCarousel from '@/components/Layout/AppCarousel'
+import AppFooter from '@/components/Layout/AppFooter'
+export default {
+  components: {
+    AppToTop,
+    AppTopNav,
+    AppHeader,
+    AppCarousel,
+    AppFooter
+  },
+  data() {
+    return {
+      isDark: false,
+      officeTime: 'Senin-Sabtu: 08.00-16.00',
+      socialNetwork: [
+        {
+          name: 'facebook',
+          link: 'https://www.facebook.com/airportcityresidence.mks/'
+        },
+        {
+          name: 'instagram',
+          link: 'https://www.instagram.com/airportcityresidence.mks/'
+        },
+        {
+          name: 'youtube',
+          link: 'https://www.youtube.com/channel/UCIpvGTTLU-eGBCM7iWCPvrg'
+        }
+      ],
+      logo: '/logo.png',
+      menu: [
+        { path: '/tentang-kami', name: 'Tentang Kami', icon: 'user-friends' },
+        { path: '/properti', name: 'Properti', icon: 'house-user' },
+        { path: '/artikel', name: 'Artikel', icon: 'newspaper' }
+      ],
+      contact: {
+        address: 'Jl. Ayah Syeh Yusuf, Moncongloe Lappara, Kec. Moncongloe',
+        region: 'Sulawesi Selatan',
+        postalCode: 90212,
+        phone: '+62 811-757575-6',
+        email: 'info@medinaland.id'
+      }
+    }
+  },
+  mounted() {
+    this.initColorScheme()
+  },
+  methods: {
+    initColorScheme() {
+      if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
+        const darkModeMediaQuery = window.matchMedia(
+          '(prefers-color-scheme: dark)'
+        )
+        if (darkModeMediaQuery.matches) {
+          this.isDark = true
+        }
+        darkModeMediaQuery.addListener((e) => {
+          const darkModeOn = e.matches
+          this.isDark = darkModeOn
+        })
+      }
+    }
+  },
+  head() {
+    return {
+      // title: this.$t('description'),
+      bodyAttrs: {
+        class: 'dark'
+        // class: this.isDark ? 'dark' : 'light'
+      }
+    }
+  }
 }
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
+</script>
