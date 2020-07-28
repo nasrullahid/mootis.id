a7687ee4fb0629bcac1f99c2b91870dbe5884fdf
Nasrullah <naz.cules@gmail.com>
2020-07-28 13:45:17 +0800

    component: re-styling blog & post component

--- components/AppBlog.vue
+++ components/AppBlog.vue
@@ -3,32 +3,27 @@
     <article>
       <div class="blog-item__img">
         <app-img :src="img" :alt="title" />
+        <div class="blog-item__date">
+          <font-awesome-icon :icon="['fas', 'user']" class="icon" />
+          <span>Admin</span>
+          <font-awesome-icon :icon="['fas', 'clock']" class="icon" />
+          <span>
+            <time :datetime="postedDate">
+              {{ formatDate(postedDate) }}
+            </time>
+          </span>
+          <!-- <font-awesome-icon :icon="['fas', 'book-reader']" class="icon" /> -->
+          <!-- <span>
+              {{ Math.ceil(readingTime.minutes.toFixed(2)) }}
+              {{ $t('minRead') }}
+            </span> -->
+        </div>
       </div>
       <div class="blog-item__meta">
         <header>
           <h2 class="blog-item__title">
             {{ title }}
           </h2>
-          <div class="blog-item__date">
-            <span>
-              <time :datetime="postedDate">
-                Posted on
-                {{ formatDate(postedDate) }}
-              </time>
-            </span>
-            •
-            <span>
-              <time :datetime="updatedDate">
-                Updated on
-                {{ formatDate(updatedDate) }}
-              </time>
-            </span>
-            •
-            <!-- <span>
-              {{ Math.ceil(readingTime.minutes.toFixed(2)) }}
-              {{ $t('minRead') }}
-            </span> -->
-          </div>
         </header>
         <p class="blog-item__summary">
           {{ summary }}
@@ -95,16 +90,30 @@ export default {
     @apply shadow-lg;
   }
 
+  &__img {
+    @apply relative;
+  }
+
   &__meta {
-    @apply p-6;
+    @apply p-4;
   }
 
   &__title {
-    @apply capitalize font-bold text-2xl mt-0 mb-4;
+    @apply capitalize font-bold text-2xl mt-0 mb-4 leading-normal;
   }
 
   &__date {
-    @apply leading-normal mb-4 text-base;
+    @apply leading-normal p-2 text-sm absolute bg-blue-900 bg-opacity-75 text-white;
+    bottom: 0;
+    right: 0;
+
+    .icon {
+      @apply mr-1 text-xs;
+    }
+
+    span {
+      @apply mr-2;
+    }
   }
 
   &__summary {
--- components/AppPost.vue
+++ components/AppPost.vue
@@ -1,37 +1,85 @@
 <template>
-  <article class="postColumn h-auto">
-    <div class="thumbnail">
-      <app-img src="/slide-1.jpg" alt="Catalya Gardens Hertasning" />
+  <article class="post h-auto">
+    <div class="post__thumbnail">
+      <app-img :src="thumbnail" :alt="title" />
+      <div class="post__address">
+        <font-awesome-icon :icon="['fas', 'map-marker-alt']" class="icon" />
+        <span>{{ address }}</span>
+      </div>
     </div>
-    <h2>Catalya Gardens Hertasning</h2>
-    <div class="address">
-      <font-awesome-icon :icon="['fas', 'map-marker-alt']" class="icon" />
-      <span>Kawasan Hertasning Makassar</span>
-    </div>
-    <div class="content">
-      Perumahan Catalya Gardens dibangun dengan Konsep Islamic Green Living
-      Concept dengan Full Fasilitas, Rumah Ekslusive Anda dengan Harga hemat dan
-      berkualitas...
-    </div>
+    <h2 class="post__title">{{ title }}</h2>
+    <p class="post__content">
+      {{ summary }}
+    </p>
+    <nuxt-link :aria-label="title" :to="slug" class="post__link">
+      {{ title }}
+    </nuxt-link>
   </article>
 </template>
 
-<style lang="postcss" scoped>
-.postColumn {
-  @apply rounded shadow overflow-hidden relative;
-  background-color: var(--card-bg);
-  & h2 {
-    @apply m-0 p-4 shadow-lg;
-    color: var(--text-normal);
-  }
-  & .address {
-    @apply px-4 text-sm border-b border-t border-gray-500 text-gray-500;
-    & span {
-      @apply ml-2;
+<script>
+export default {
+  name: 'AppPost',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    thumbnail: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    slug: {
+      type: String,
+      required: true
+    },
+    summary: {
+      type: String,
+      required: true
     }
   }
-  & .content {
-    @apply p-4;
+}
+</script>
+
+<style lang="postcss" scoped>
+.post {
+  @apply rounded shadow overflow-hidden relative max-w-2xl;
+  background-color: var(--card-bg);
+
+  &:hover {
+    @apply shadow-lg;
+  }
+
+  &__thumbnail {
+    @apply relative;
+  }
+
+  &__address {
+    @apply leading-normal p-2 text-sm absolute bg-blue-900 bg-opacity-75 text-white w-full;
+    bottom: 0;
+    left: 0;
+
+    .icon {
+      @apply mr-2;
+    }
+  }
+
+  &__title {
+    @apply m-0 p-4 text-center leading-normal capitalize font-bold text-2xl;
+    color: var(--text-normal);
+  }
+
+  &__content {
+    @apply p-4 shadow m-0 leading-normal;
+  }
+
+  &__link {
+    @apply absolute top-0 left-0 w-full h-full overflow-hidden z-0;
+    text-indent: -9999px;
   }
 }
 </style>
