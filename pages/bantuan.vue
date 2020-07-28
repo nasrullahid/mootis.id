9d375df506e4c936f8194e0611be2d7418478154
Nasrullah <naz.cules@gmail.com>
2020-07-28 13:46:31 +0800

    pages: create new static page

---
+++ pages/bantuan.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="static-page">
+    <AppBreadcrumb title="Pertanyaan Yang Sering Diajukan" />
+    <div class="max-w-4xl ml-auto mr-auto py-12 px-4 h-screen"></div>
+  </div>
+</template>
+
+<script>
+import AppBreadcrumb from '@/components/Layout/AppBreadcrumb'
+import { HOSTNAME } from '~/constant'
+export default {
+  name: 'Bantuan',
+  components: { AppBreadcrumb },
+  head() {
+    return {
+      title: 'Pertanyaan Yang Sering Diajukan',
+      meta: [
+        {
+          hid: 'og:url',
+          name: 'og:url',
+          property: 'og:url',
+          content: `${HOSTNAME}/bantuan`
+        }
+      ]
+    }
+  }
+}
+</script>
