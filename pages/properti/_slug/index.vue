19daa908e52635da93e069d0cb791edea992e9c8
Nasrullah <naz.cules@gmail.com>
2020-07-29 15:00:48 +0800

    pages: update page properti item

--- pages/properti/_slug/index.vue
+++ pages/properti/_slug/index.vue
@@ -2,7 +2,7 @@
   <div class="page-post">
     <div class="banner">
       <div class="banner__img">
-        <app-img :src="blog.img" :alt="blog.title" />
+        <app-img :src="`/properti/${post.banner}`" :alt="post.title" />
       </div>
     </div>
     <div class="max-w-4xl mx-auto pb-12 px-4">
@@ -17,37 +17,142 @@
           </client-only>
         </div>
 
-        <h2 class="post__title">{{ blog.title }}</h2>
+        <h2 class="post__title">{{ post.title }}</h2>
 
         <div class="post__date">
           <font-awesome-icon :icon="['fas', 'map-marker-alt']" class="icon" />
-          <span>Moncongloe Maros Sulawesi Selatan</span>
+          <span>{{ post.address }}</span>
+          <font-awesome-icon :icon="['fas', 'clock']" class="icon" />
+          <span>
+            <time :datetime="post.postedDate">
+              {{ formatDate(post.postedDate) }}
+            </time>
+          </span>
         </div>
 
-        <h4 class="post__subtitle">Islamic Green Living Concept</h4>
-        <div class="post__description">
-          <p class="mb-0">
-            Al Jazeera Residence Moncongloe, hunian islami eksklusif di Sulawesi
-            Selatan yang menghadirkan konsep islami modern eksklusif, dengan
-            desain arsitektur minimalis modern dan menedepankan aspek green
-            living concept yang religius, humanis, dan prestisius
-          </p>
-        </div>
-        <div class="post_fasilitas">
-          <h3>Fasilitas yang Anda Dapatkan</h3>
-          <ul>
-            <li>One gate system</li>
-            <li>One home one hafidz</li>
-          </ul>
-        </div>
-        <div class="post_siteplan">
-          <h3>Site Plan {{ blog.title }}</h3>
-        </div>
-        <div class="post_typeunit">
-          <h3>Tipe Unit &amp; Spesifikasi</h3>
-        </div>
-        <div class="post_maps">
-          <h3>Lihat Lokasi di Google Maps</h3>
+        <div class="post__content">
+          <h4 class="post__content__heading">{{ post.headline }}</h4>
+
+          <div class="post__content__description">
+            <p class="mb-0">{{ post.description }}</p>
+          </div>
+
+          <div class="post__content__fasilitas">
+            <h3 class="mb-4">Fasilitas yang Anda Dapatkan</h3>
+            <ul>
+              <li>One gate system</li>
+              <li>One home one hafidz</li>
+            </ul>
+          </div>
+
+          <div class="post__content__siteplan">
+            <h3 class="mb-4">Site Plan {{ post.title }}</h3>
+            <app-img
+              :src="`/properti/${post.sitePlan}`"
+              :alt="`Site Plan ${post.title}`"
+            />
+          </div>
+
+          <div class="post__content__typeunit">
+            <h3 class="mb-4">Tipe Unit &amp; Spesifikasi</h3>
+            <div v-for="(item, index) in post.tipeUnit" :key="index">
+              <h5 class="m-0">{{ index + 1 }}. {{ item.tipe }}</h5>
+              <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <app-img :src="`/properti/${item.image}`" :alt="item.tipe" />
+                <div class="spesifikasi">
+                  <p>Spesifikasi {{ item.tipe }}</p>
+                  <ul>
+                    <li v-for="spesc in item.spesifikasi" :key="spesc">
+                      {{ spesc }}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="post__content__maps">
+            <h3 class="mb-4">Lihat Lokasi di Google Maps</h3>
+            <figure class="video">
+              <div class="embed embed__16/9">
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7947.453144478817!2d119.542629!3d-5.147647!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5b3a127daa753612!2sAl%20Jazeera%20Residence!5e0!3m2!1sen!2sid!4v1595999628401!5m2!1sen!2sid"
+                ></iframe>
+              </div>
+            </figure>
+          </div>
+
+          <div class="post__content__form">
+            <h3 class="mb-4">Booking Segera Sebelum Kehabisan</h3>
+            <p class="text-sm italic mb-4">
+              Mohon isi formulir dengan data yang benar untuk mempermudah proses
+              pemesanan Anda
+            </p>
+            <form class="w-full max-w-xl">
+              <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                  <label class="md:text-right md:mb-0" for="name">
+                    Nama Lengkap
+                  </label>
+                </div>
+                <div class="md:w-2/3">
+                  <input
+                    id="name"
+                    class="focus:outline-none focus:bg-white focus:border-blue-500"
+                    type="text"
+                    placeholder="Nasrullah"
+                  />
+                </div>
+              </div>
+              <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                  <label class="md:text-right md:mb-0" for="phone">
+                    No. HP/WA
+                  </label>
+                </div>
+                <div class="md:w-2/3">
+                  <input
+                    id="phone"
+                    class="focus:outline-none focus:bg-white focus:border-blue-500"
+                    type="number"
+                    placeholder="62823456789xx"
+                  />
+                </div>
+              </div>
+              <div class="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                  <label class="md:text-right md:mb-0" for="city">
+                    Kota/Kabupaten
+                  </label>
+                </div>
+                <div class="md:w-2/3">
+                  <input
+                    id="city"
+                    class="focus:outline-none focus:bg-white focus:border-blue-500"
+                    type="text"
+                    placeholder="Makassar"
+                  />
+                </div>
+              </div>
+              <div class="md:flex md:items-center">
+                <div class="md:w-1/3"></div>
+                <div class="md:w-2/3">
+                  <button
+                    class="bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none mb-2 w-full sm:mb-0 sm:w-1/2"
+                    type="button"
+                  >
+                    Pesan Sekarang
+                  </button>
+                  <button
+                    class="bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none w-full sm:w-auto"
+                    type="button"
+                  >
+                    Pesan via Whatsapp
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </article>
     </div>
@@ -62,14 +167,13 @@
 </template>
 
 <script>
-import readingTime from 'reading-time'
 import { formatDate } from '~/mixins'
 import { HOSTNAME } from '~/constant'
 export default {
   mixins: [formatDate],
   data() {
     return {
-      blog: null,
+      post: null,
       slickOptions: {
         arrows: true,
         dots: true,
@@ -91,21 +195,47 @@ export default {
   },
   created() {
     const slug = this.$route && this.$route.params && this.$route.params.slug
-    const fullPath = `${HOSTNAME}/artikel/${slug}`
-    const content =
-      "<h3>What is Lorem Ipsum?</h3><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><h3>Why do we use it?</h3><p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p><h3>Where does it come from?</h3><p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.</p><p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p><h3>Where can I get some?</h3><p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>"
+    const fullPath = `${HOSTNAME}/properi/${slug}`
 
-    this.blog = {
-      img: '/slide-1.jpg',
+    this.post = {
+      banner: 'slide-1.jpg',
       title: 'Al Jazeera Residence Moncongloe',
+      headline: 'Islamic Green Living Concept',
       description:
-        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...',
-      postedDate: '2020-07-27T11:00:00.000Z',
-      updatedDate: '2020-07-27T11:00:00.000Z',
+        'Al Jazeera Residence Moncongloe, hunian islami eksklusif di Sulawesi Selatan yang menghadirkan konsep islami modern eksklusif, dengan desain arsitektur minimalis modern dan menedepankan aspek green living concept yang religius, humanis, dan prestisius',
+      address: 'Moncongloe Maros Sulawesi Selatan',
+      postedDate: new Date(),
+      updatedDate: new Date(),
+      sitePlan: 'siteplan.jpg',
+      tipeUnit: [
+        {
+          tipe: 'Tipe 48/60',
+          image: 'tipeunit-48.jpeg',
+          spesifikasi: [
+            'Garasi Mobil',
+            'Ruang Tamu',
+            'Ruang Keluarga',
+            'Dapur + Ruang Makan',
+            '2 Kamar Mandi / WC',
+            '3 Kamar Tidur',
+            '1 Balkon'
+          ]
+        },
+        {
+          tipe: 'Tipe 68/72',
+          image: 'tipeunit-68.jpeg',
+          spesifikasi: [
+            'Garasi Mobil',
+            'Ruang Tamu',
+            'Ruang Keluarga',
+            'Dapur + Ruang Makan',
+            '2 Kamar Mandi / WC',
+            '3 Kamar Tidur',
+            '2 Balkon'
+          ]
+        }
+      ],
       slug,
-      contributors: 'Nasrullah',
-      content,
-      readingTime: readingTime(content),
       fullPath
     }
   },
@@ -155,10 +285,6 @@ export default {
     @apply leading-normal p-8 text-center font-bold text-3xl m-0;
   }
 
-  &__subtitle {
-    @apply leading-normal p-8 font-bold text-2xl m-0;
-  }
-
   &__date {
     @apply leading-normal py-2 px-8 text-sm bg-blue-900 bg-opacity-75 text-white;
 
@@ -171,27 +297,90 @@ export default {
     }
   }
 
-  &__description {
-    @apply m-0 px-8;
-    p {
-      @apply shadow bg-gray-600 rounded-lg px-4 py-6 text-center;
-    }
-  }
-}
+  &__content {
+    @apply p-8;
 
-@screen sm {
-  .post {
-    &__meta {
-      @apply px-16 py-10;
+    &__heading {
+      @apply leading-normal font-bold text-2xl m-0 mb-4;
+    }
+
+    &__description {
+      @apply m-0;
+      p {
+        @apply shadow bg-gray-600 rounded-lg px-4 py-6 text-center;
+      }
+    }
+
+    &__fasilitas {
+      ul li {
+        @apply m-0;
+      }
+    }
+
+    &__siteplan {
+      .image-placeholder {
+        @apply max-w-2xl mx-auto shadow rounded;
+      }
+    }
+
+    &__typeunit {
+      h5 {
+        @apply mb-4 border-b text-base;
+        border-color: var(--inline-code-border);
+      }
+      div:not(:last-child) {
+        @apply mb-4;
+      }
+      .image-placeholder {
+        @apply w-full shadow;
+      }
+      .spesifikasi {
+        @apply w-full h-full p-4 shadow;
+        background: var(--card-bg);
+        p {
+          @apply font-bold text-base mb-4;
+        }
+        ul {
+          @apply mb-0;
+        }
+        li {
+          @apply m-0 text-sm;
+        }
+      }
+    }
+
+    &__form {
+      form {
+        label {
+          @apply block text-gray-500 mb-1 pr-4 text-base;
+        }
+        input {
+          @apply bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight text-base;
+        }
+        button {
+          @apply shadow text-white py-2 px-4 rounded text-base;
+        }
+      }
     }
   }
 }
 
 body {
   &.light {
-    .post__description {
+    .post__content__description {
       p {
         @apply text-white;
+      }
+    }
+  }
+
+  &.dark {
+    .post {
+      input {
+        filter: brightness(0.9);
+      }
+      button {
+        filter: brightness(0.9);
       }
     }
   }
