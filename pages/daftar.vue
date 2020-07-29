df780444356cd921609c71e47b6c4d27afb933c9
Nasrullah <naz.cules@gmail.com>
2020-07-29 21:05:04 +0800

    pages: create page login & daftar

---
+++ pages/daftar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="static-page">
+    <AppBreadcrumb title="Daftar Jadi Marketing" />
+    <div class="max-w-4xl ml-auto mr-auto py-12 px-4 h-screen"></div>
+  </div>
+</template>
+
+<script>
+import AppBreadcrumb from '@/components/Layout/AppBreadcrumb'
+import { HOSTNAME } from '~/constant'
+export default {
+  name: 'Daftar',
+  components: { AppBreadcrumb },
+  mounted() {
+    this.$fb.enable()
+  },
+  head() {
+    return {
+      title: 'Daftar Jadi Marketing',
+      meta: [
+        {
+          hid: 'og:url',
+          name: 'og:url',
+          property: 'og:url',
+          content: `${HOSTNAME}/bantuan`
+        }
+      ]
+    }
+  }
+}
+</script>
