91a7ea31e684cb1de574ededdacb56bba090d09b
Nasrullah <naz.cules@gmail.com>
2020-07-31 00:07:29 +0800

    constant: Create environment

---
+++ constant/env.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+export default {
+  // Company
+  hostname: 'https://mootis.nasrullah.id',
+  appName: 'MOOTIS.ID',
+  companyName: 'Megah Developer Properti Syariah Indonesia',
+  description:
+    'Megah Developer Properti Syariah Indonesia (mootis.id) pelopor aplikasi markeing properti dengan konsep penjualan sesuai dengan kaidah syariah Islam',
+  keywords:
+    'mootis, properti, syariah, tanpa bank, tanpa denda, tanpa riba, tanpa sita, tanpa akad bathil, properti syariah',
+  creatorName: 'Nasrullah',
+  creatorAlternate: 'nazcules',
+
+  // backend (API)
+  baseUrl: process.env.BASE_URL,
+
+  // Google
+  googleAnalytics: 'UA-85283525-3',
+  gsiteVerification: '4MePSYJQJNLm6NzDdIoiY8fctFdyloVjPdgec1TSSeM',
+
+  // Twitter
+  twitterSite: '@mootis.id',
+  twitterCreator: '@nasrullahID',
+
+  // Open Graph (Facebook)
+  fbPixelId: '330609454291424',
+  fbAdminId: '100000309115621',
+  fbAppId: '962454547510889',
+  fbPageId: '1148586411880482'
+}
